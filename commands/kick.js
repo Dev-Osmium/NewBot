db13475750cd8ff25311c851de95425b778e24fb
Dev-Osmium <oatmas64134@gmail.com>
2017-09-05 15:20:27 -0400

    Update Kick

--- commands/kick.js
+++ commands/kick.js
@@ -1,21 +1,22 @@
-const config = require('../config.json');
-
+const config = require('../config.json')
 exports.run = (client, message, [mention, ...reason]) => {
-	const modRole = config.modRole;
-	if(!modRole)
-		return message.reply(`No mod role exists! Please add this role, ${modRole}`)
-	
-	if (!message.member.roles.has(modRole.id))
-    	return message.reply("You can't use this command.");
-    if (message.mentions.users.size === 0) {
-    	return message.reply("Please mention a user to kick");
+  const modRole = message.guild.roles.find("name", config.modRole);
+  if (!modRole)
+    return console.log("The Mods role does not exist");
+
+  if (!message.member.roles.has(modRole.id))
+    return message.reply("You can't use this command.");
+
+  if (message.mentions.users.size === 0) {
+    return message.reply("Please mention a user to kick");
+
     if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-   		return message.reply("I need the KICK_MEMBERS permission!");
+      return message.reply("");
 
-   	const kickMember = message.mentions.members.first();
+    const kickMember = message.mentions.members.first();
 
-   	kickMember.kick(reason.join(" ")).then(member => {
-    	message.reply(`${member.user.username} was succesfully kicked.`);
+    kickMember.kick(reason.join(" ")).then(member => {
+      message.reply(`${member.user.username} was succesfully kicked.`);
     });
-
+  }
 };
